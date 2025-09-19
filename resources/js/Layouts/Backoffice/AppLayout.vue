b637e914c52c1f656f97e2fc46cbf1dfd0961e7e
Fyrmansyah <firmansyahrizki278@gmail.com>
2025-09-19 21:01:49 +0700

    feat(backoffice): WIP layouting

---
+++ resources/js/Layouts/Backoffice/AppLayout.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+    import NavHeader from "./NavHeader.vue";
+    import SidebarMenu from "./SidebarMenu.vue";
+</script>
+
+<template>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <SidebarMenu />
+
+            <!-- Layout container -->
+            <div class="layout-page">
+                <NavHeader />
+
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    <!-- Content -->
+
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        <slot></slot>
+                    </div>
+                    <!-- / Content -->
+
+                    <div class="content-backdrop fade"></div>
+                </div>
+                <!-- Content wrapper -->
+            </div>
+            <!-- / Layout page -->
+        </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
+</template>
