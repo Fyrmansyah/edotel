f643c47b48725b17f8af8e4f0562422fac1f3ee7
Fyrmansyah <firmansyahrizki278@gmail.com>
2025-10-04 18:47:32 +0700

    wip: add route_api mapping for js

---
+++ resources/js/Routes/Api.js
@@ -0,0 +1,16 @@
+export const ROUTE_API = {
+    client: {},
+    admin: {
+        payment_methods: {
+            update_qris: "/admin/payment-methods/qris",
+        },
+        pricings: {
+            index: "/admin/pricings",
+            update_pricing: (pricingId) => `/admin/pricings/${pricingId}`,
+        },
+        bookings: {
+            index: "/admin/index",
+            update_status: (bookingId) => `/admin/bookings/update/${bookingId}`,
+        },
+    },
+};
