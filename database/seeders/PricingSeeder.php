621bc54adc0f9f8f676b4471bda88ae9ff256114
Fyrmansyah <firmansyahrizki278@gmail.com>
2025-10-08 14:29:05 +0700

    feat(pricing): update harga kamar large price in seeder

--- database/seeders/PricingSeeder.php
+++ database/seeders/PricingSeeder.php
@@ -14,7 +14,7 @@ class PricingSeeder extends Seeder
     public function run(): void
     {
         Pricing::create(['name' => 'Harga Kamar - Medium', 'value' => 150000]);
-        Pricing::create(['name' => 'Harga Kamar - Large', 'value' => 150000]);
+        Pricing::create(['name' => 'Harga Kamar - Large', 'value' => 300000]);
         Pricing::create(['name' => 'Extra Makan', 'value' => 50000]);
         Pricing::create(['name' => 'Extra Kasur', 'value' => 75000]);
     }
