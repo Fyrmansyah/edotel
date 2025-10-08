5c09767e327ff9255985c059912c71c787263a37
Fyrmansyah <firmansyahrizki278@gmail.com>
2025-10-08 15:50:59 +0700

    feat(pricing): add pricing key constant

---
+++ resources/js/Helpers/constant.js
@@ -0,0 +1,6 @@
+export const PRICING_KEY = {
+    kamar_medium: "Harga Kamar - Medium",
+    kamar_large: "Harga Kamar - Large",
+    extra_makan: "Extra Makan",
+    extra_kasur: "Extra Kasur",
+};
