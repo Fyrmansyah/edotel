77674f3aaa6ec433d3ae8840750e6bf2a2a09b83
Fyrmansyah <firmansyahrizki278@gmail.com>
2025-10-08 21:11:47 +0700

    feat(client-ui): add option without border for BookingDetailItem

---
+++ resources/js/Components/Client/Booking/BookingDetailItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <tr v-bind="$attrs">
+        <td
+            class="align-baseline !border-b !border-b-gray-200 py-1 whitespace-nowrap"
+            :class="{ 'border-none': noBorder }"
+        >
+            {{ text1 }}
+        </td>
+        <td
+            class="align-baseline !border-b !border-b-gray-200 py-1 px-3"
+            :class="{ 'border-none': noBorder }"
+        >
+            :
+        </td>
+        <td
+            class="align-baseline !border-b !border-b-gray-200 py-1 w-full"
+            :class="{ 'border-none': noBorder }"
+        >
+            {{ text2 }}
+        </td>
+    </tr>
+</template>
+
+<script setup>
+    defineProps({ text1: String, text2: String, noBorder: Boolean });
+</script>
